a1a751712974c86e54dc62f3f1615eed370ec549
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-01-02 18:43:02 +0100

    increase default map donwload speed

--- src/engine/shared/config_variables.h
+++ src/engine/shared/config_variables.h
@@ -82,7 +82,7 @@ MACRO_CONFIG_INT(SvExternalPort, sv_external_port, 0, 0, 0, CFGFLAG_SAVE|CFGFLAG
 MACRO_CONFIG_STR(SvMap, sv_map, 128, "BlmapChill", CFGFLAG_SAVE|CFGFLAG_SERVER, "Map to use on the server", AUTHED_ADMIN)
 MACRO_CONFIG_INT(SvMaxClients, sv_max_clients, 64, 1, MAX_CLIENTS, CFGFLAG_SAVE|CFGFLAG_SERVER, "Maximum number of clients that are allowed on a server", AUTHED_ADMIN)
 MACRO_CONFIG_INT(SvMaxClientsPerIP, sv_max_clients_per_ip, 4, 1, MAX_CLIENTS, CFGFLAG_SAVE|CFGFLAG_SERVER, "Maximum number of clients with the same IP that can connect to the server", AUTHED_ADMIN)
-MACRO_CONFIG_INT(SvMapDownloadSpeed, sv_map_download_speed, 8, 1, 16, CFGFLAG_SAVE|CFGFLAG_SERVER, "Number of map data packages a client gets on each request", AUTHED_ADMIN)
+MACRO_CONFIG_INT(SvMapDownloadSpeed, sv_map_download_speed, 16, 1, 16, CFGFLAG_SAVE|CFGFLAG_SERVER, "Number of map data packages a client gets on each request", AUTHED_ADMIN)
 MACRO_CONFIG_INT(SvHighBandwidth, sv_high_bandwidth, 0, 0, 1, CFGFLAG_SAVE|CFGFLAG_SERVER, "Use high bandwidth mode. Doubles the bandwidth required for the server. LAN use only", AUTHED_ADMIN)
 MACRO_CONFIG_INT(SvRegister, sv_register, 1, 0, 1, CFGFLAG_SAVE|CFGFLAG_SERVER, "Register server with master server for public listing", AUTHED_ADMIN)
 MACRO_CONFIG_STR(SvRconPassword, sv_rcon_password, 32, "", CFGFLAG_SAVE|CFGFLAG_SERVER|CFGFLAG_NONTEEHISTORIC, "Remote console password (full access)", AUTHED_ADMIN)
