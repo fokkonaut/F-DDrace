e35c58e0283dc2cba645ff9c5cb9c584578dd93d
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-01-12 02:52:05 +0100

    Add this

--- src/game/server/shop.cpp
+++ src/game/server/shop.cpp
@@ -3,7 +3,7 @@
 #include "shop.h"
 
 // manually checked amount of newlines between the end of the description of the current page and the footer
-int pNumNewLines[NUM_ITEMS_LIST+2] = { 13, 10, 10, 8, 7, 8, 11, 9, 9, 9, 10, 9, 12, 17 };
+int pNumNewLines[NUM_ITEMS_LIST+2] = { 13, 10, 10, 8, 7, 8, 11, 9, 9, 9, 10, 9, 14, 17 };
 
 CShop::CShop(CGameContext *pGameServer)
 {
@@ -293,7 +293,7 @@ void CShop::ConfirmPurchase(int ClientID)
 	str_format(aBuf, sizeof(aBuf),
 		"Are you sure you want to buy this item?\n\n"
 		"F3 - yes\n"
-		"F4 - no\n\n");
+		"F4 - no");
 
 	m_pGameServer->SendMotd(FormatMotd(aBuf, -1), ClientID);
 	m_PurchaseState[ClientID] = STATE_CONFIRM;
