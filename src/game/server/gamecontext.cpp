c0d10f8da2ccd88ce8304971bb9b3e5f34a76c7d
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-07-09 13:21:48 +0200

    Small things

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -1502,22 +1502,15 @@ void *CGameContext::PreProcessMsg(int MsgID, CUnpacker *pUnpacker, int ClientID)
 
 			CNetMsg_Cl_SkinChange *pMsg = (CNetMsg_Cl_SkinChange *)s_aRawMsg;
 
-			const char *pName = "";
-			const char *pClan = "";
-			int Country = -1;
+			const char *pName = pUnpacker->GetString(CUnpacker::SANITIZE_CC|CUnpacker::SKIP_START_WHITESPACES);
+			const char *pClan = pUnpacker->GetString(CUnpacker::SANITIZE_CC|CUnpacker::SKIP_START_WHITESPACES);
+			int Country = pUnpacker->GetInt();
 
-			pName = pUnpacker->GetString(CUnpacker::SANITIZE_CC|CUnpacker::SKIP_START_WHITESPACES);
-			pClan = pUnpacker->GetString(CUnpacker::SANITIZE_CC|CUnpacker::SKIP_START_WHITESPACES);
-			Country = pUnpacker->GetInt();
+			str_copy(pPlayer->m_TeeInfos.m_Sevendown.m_SkinName, pUnpacker->GetString(CUnpacker::SANITIZE_CC|CUnpacker::SKIP_START_WHITESPACES), sizeof(pPlayer->m_TeeInfos.m_Sevendown.m_SkinName));
+			pPlayer->m_TeeInfos.m_Sevendown.m_UseCustomColor = pUnpacker->GetInt() ? 1 : 0;
+			pPlayer->m_TeeInfos.m_Sevendown.m_ColorBody = pUnpacker->GetInt();
+			pPlayer->m_TeeInfos.m_Sevendown.m_ColorFeet = pUnpacker->GetInt();
 
-			CPlayer::TeeInfos::Sevendown Sevendown = { "", 0, 0, 0 };
-
-			str_copy(Sevendown.m_SkinName, pUnpacker->GetString(CUnpacker::SANITIZE_CC|CUnpacker::SKIP_START_WHITESPACES), sizeof(Sevendown.m_SkinName));
-			Sevendown.m_UseCustomColor = pUnpacker->GetInt() ? 1 : 0;
-			Sevendown.m_ColorBody = pUnpacker->GetInt();
-			Sevendown.m_ColorFeet = pUnpacker->GetInt();
-
-			pPlayer->m_TeeInfos.m_Sevendown = Sevendown;
 			m_Skins.SkinFromSevendown(&pPlayer->m_TeeInfos);
 
 			for (int p = 0; p < NUM_SKINPARTS; p++)
