6fd172765d0addf7504f27f0d2630b595d1be92a
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-07-13 00:39:48 +0200

    better

--- src/game/server/ddracecommands.cpp
+++ src/game/server/ddracecommands.cpp
@@ -1274,13 +1274,12 @@ void CGameContext::ConPlayerSkin(IConsole::IResult* pResult, void* pUserData)
 	if (pResult->NumArguments() > 1)
 	{
 		CTeeInfo Info(pResult->GetString(1));
-		Info.ToSevendown();
 
 		CPlayer* pFrom = pSelf->m_apPlayers[pResult->GetInteger(1)];
 		char aInteger[4];
 		str_format(aInteger, sizeof(aInteger), "%d", pResult->GetInteger(1));
 
-		if (pFrom && !str_comp_nocase(aInteger, pResult->GetString(1)))
+		if (pFrom && Info.m_SkinID == -1 && !str_comp_nocase(aInteger, pResult->GetString(1)))
 			pSelf->SendSkinChange(pFrom->m_TeeInfos, Victim, -1);
 		else
 			pPlayer->SetSkin(Info.m_SkinID, true);
