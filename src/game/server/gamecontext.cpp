ea3d24dfd0dc46059812d6e96ee865c9ce261aed
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-07-31 01:45:01 +0200

    remove test

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -2954,7 +2954,6 @@ void CGameContext::OnInit()
 	m_World.SetGameServer(this);
 	m_Events.SetGameServer(this);
 	m_CommandManager.Init(m_pConsole, this, NewCommandHook, RemoveCommandHook);
-	Config()->m_SvTestingCommands = 1;
 
 	m_GameUuid = RandomUuid();
 	Console()->SetTeeHistorianCommandCallback(CommandCallback, this);
