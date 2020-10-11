812cf1d94a3a753944f256031f53205ef4972c08
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-10-11 17:43:29 +0200

    Forgot about these

--- src/game/collision.cpp
+++ src/game/collision.cpp
@@ -369,16 +369,15 @@ int CCollision::IntersectLine(vec2 Pos0, vec2 Pos1, vec2* pOutCollision, vec2* p
 
 int CCollision::IntersectLineTeleHook(vec2 Pos0, vec2 Pos1, vec2* pOutCollision, vec2* pOutBeforeCollision, int* pTeleNr)
 {
-	float Distance = distance(Pos0, Pos1);
-	int End(Distance + 1);
+	const int End = distance(Pos0, Pos1)+1;
+	const float InverseEnd = 1.0f/End;
 	vec2 Last = Pos0;
 	int ix = 0, iy = 0; // Temporary position for checking collision
 	int dx = 0, dy = 0; // Offset for checking the "through" tile
 	ThroughOffset(Pos0, Pos1, &dx, &dy);
 	for (int i = 0; i <= End; i++)
 	{
-		float a = i / (float)End;
-		vec2 Pos = mix(Pos0, Pos1, a);
+		vec2 Pos = mix(Pos0, Pos1, i*InverseEnd);
 		ix = round_to_int(Pos.x);
 		iy = round_to_int(Pos.y);
 
@@ -426,14 +425,13 @@ int CCollision::IntersectLineTeleHook(vec2 Pos0, vec2 Pos1, vec2* pOutCollision,
 
 int CCollision::IntersectLineTeleWeapon(vec2 Pos0, vec2 Pos1, vec2* pOutCollision, vec2* pOutBeforeCollision, int* pTeleNr)
 {
-	float Distance = distance(Pos0, Pos1);
-	int End(Distance + 1);
+	const int End = distance(Pos0, Pos1)+1;
+	const float InverseEnd = 1.0f/End;
 	vec2 Last = Pos0;
 	int ix = 0, iy = 0; // Temporary position for checking collision
 	for (int i = 0; i <= End; i++)
 	{
-		float a = i / (float)End;
-		vec2 Pos = mix(Pos0, Pos1, a);
+		vec2 Pos = mix(Pos0, Pos1, i*InverseEnd);
 		ix = round_to_int(Pos.x);
 		iy = round_to_int(Pos.y);
 
