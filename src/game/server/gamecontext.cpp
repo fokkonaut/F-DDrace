d5fd3e21a0da68a67bc7e396abb9e1b0fde4fa4d
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2020-11-08 15:11:33 +0100

    remove testing

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -807,8 +807,6 @@ void CGameContext::SendTuningParams(int ClientID, int Zone)
 
 void CGameContext::OnTick()
 {
-	Config()->m_SvAllowSevendown = 1;
-	Config()->m_SvTestingCommands = 1;
 	if(m_TeeHistorianActive)
 	{
 		if(!m_TeeHistorian.Starting())
