d770ac4a9a82eb106a9bc64d5abd7a17a37738c2
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2021-01-10 23:43:26 +0100

    Fix

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -4275,7 +4275,7 @@ void CGameContext::RemovePortalsFromPlot(int PlotID)
 		CPortal *pPortal = (CPortal *)m_World.FindFirst(CGameWorld::ENTTYPE_PORTAL);
 		for (; pPortal; pPortal = (CPortal *)pPortal->TypeNext())
 		{
-			if (GetTilePlotID(pPortal->GetPos()))
+			if (GetTilePlotID(pPortal->GetPos()) == PlotID)
 			{
 				pPortal->Reset();
 				pPortal->DestroyLinkedPortal();
