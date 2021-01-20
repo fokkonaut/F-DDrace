ace964fcd3ad9254a7e3ecb94a1d25ce59bf6658
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2021-01-20 22:31:57 +0100

    Fix rcon logout

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -2266,6 +2266,7 @@ void CServer::LogoutClient(int ClientID, const char *pReason)
 
 	m_aClients[ClientID].m_AuthTries = 0;
 	m_aClients[ClientID].m_pRconCmdToSend = 0;
+	m_aClients[ClientID].m_pMapListEntryToSend = 0;
 
 	char aBuf[64];
 	if(*pReason)
@@ -2548,23 +2549,7 @@ void CServer::ConLogout(IConsole::IResult *pResult, void *pUser)
 	if(pServer->m_RconClientID >= 0 && pServer->m_RconClientID < MAX_CLIENTS &&
 		pServer->m_aClients[pServer->m_RconClientID].m_State != CServer::CClient::STATE_EMPTY)
 	{
-		CMsgPacker Msg(NETMSG_RCON_AUTH_OFF, true);
-		if (pServer->m_aClients[pServer->m_RconClientID].m_Sevendown)
-		{
-			Msg.AddInt(0);
-			Msg.AddInt(0);
-		}
-		pServer->SendMsg(&Msg, MSGFLAG_VITAL, pServer->m_RconClientID);
-
-		pServer->m_aClients[pServer->m_RconClientID].m_Authed = AUTHED_NO;
-		pServer->m_aClients[pServer->m_RconClientID].m_AuthTries = 0;
-		pServer->m_aClients[pServer->m_RconClientID].m_pRconCmdToSend = 0;
-		pServer->m_aClients[pServer->m_RconClientID].m_pMapListEntryToSend = 0;
-		pServer->GameServer()->OnClientAuth(pServer->m_RconClientID, AUTHED_NO);
-		pServer->SendRconLine(pServer->m_RconClientID, "Logout successful.");
-		char aBuf[32];
-		str_format(aBuf, sizeof(aBuf), "ClientID=%d logged out", pServer->m_RconClientID);
-		pServer->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "server", aBuf);
+		pServer->LogoutClient(pServer->m_RconClientID, "");
 	}
 }
 
@@ -2971,11 +2956,15 @@ bool CServer::SetTimedOut(int ClientID, int OrigID)
 		return false;
 	}
 
+	if(m_aClients[OrigID].m_Authed != AUTHED_NO)
+	{
+		LogoutClient(ClientID, "Timeout Protection");
+	}
+
 	// important ot call OnSetTimedOut before we remove the original client but after we swapped already
 	GameServer()->OnSetTimedOut(ClientID, OrigID);
 
 	DelClientCallback(OrigID, "Timeout Protection used", this);
-	m_aClients[ClientID].m_Authed = AUTHED_NO;
 	//m_aClients[ClientID].m_Flags = m_aClients[OrigID].m_Flags;
 	return true;
 }
