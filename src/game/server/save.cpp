c8900bbefd7416a3f705ca30ae03d0059d1dbc54
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2022-07-11 01:03:46 +0200

    correctly save and load draw editor

--- src/game/server/save.cpp
+++ src/game/server/save.cpp
@@ -427,9 +427,10 @@ char* CSaveTee::GetString()
 		"%d\t%d\t%d\t"
 		"%d\t%d\t%d\t"
 		"%d\t%d\t%d\t"
+		"%d\t%d\t%d\t"
 		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
-		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
-		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
+		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
+		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
 		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
 		"%d\t%d\t%d\t"
 		"%d\t%d\t%lld\t%d\t%d\t%d\t"
@@ -474,11 +475,12 @@ char* CSaveTee::GetString()
 		m_aWeapons[13].m_AmmoRegenStart, m_aWeapons[13].m_Ammo, m_aWeapons[13].m_Got,
 		m_aWeapons[14].m_AmmoRegenStart, m_aWeapons[14].m_Ammo, m_aWeapons[14].m_Got,
 		m_aWeapons[15].m_AmmoRegenStart, m_aWeapons[15].m_Ammo, m_aWeapons[15].m_Got,
+		m_aWeapons[16].m_AmmoRegenStart, m_aWeapons[16].m_Ammo, m_aWeapons[16].m_Got,
 		m_Invisible, m_Rainbow, m_Atom, m_Trail, m_Meteors, m_Bloody, m_StrongBloody, m_ScrollNinja, m_HookPower,
 		m_aSpreadWeapon[0], m_aSpreadWeapon[1], m_aSpreadWeapon[2], m_aSpreadWeapon[3], m_aSpreadWeapon[4], m_aSpreadWeapon[5], m_aSpreadWeapon[6], m_aSpreadWeapon[7],
-		m_aSpreadWeapon[8], m_aSpreadWeapon[9], m_aSpreadWeapon[10], m_aSpreadWeapon[11], m_aSpreadWeapon[12], m_aSpreadWeapon[13], m_aSpreadWeapon[14], m_aSpreadWeapon[15],
+		m_aSpreadWeapon[8], m_aSpreadWeapon[9], m_aSpreadWeapon[10], m_aSpreadWeapon[11], m_aSpreadWeapon[12], m_aSpreadWeapon[13], m_aSpreadWeapon[14], m_aSpreadWeapon[15], m_aSpreadWeapon[16],
 		m_aHadWeapon[0], m_aHadWeapon[1], m_aHadWeapon[2], m_aHadWeapon[3], m_aHadWeapon[4], m_aHadWeapon[5], m_aHadWeapon[6], m_aHadWeapon[7],
-		m_aHadWeapon[8], m_aHadWeapon[9], m_aHadWeapon[10], m_aHadWeapon[11], m_aHadWeapon[12], m_aHadWeapon[13], m_aHadWeapon[14], m_aHadWeapon[15],
+		m_aHadWeapon[8], m_aHadWeapon[9], m_aHadWeapon[10], m_aHadWeapon[11], m_aHadWeapon[12], m_aHadWeapon[13], m_aHadWeapon[14], m_aHadWeapon[15], m_aHadWeapon[16],
 		m_FakeTuneCollision, m_OldFakeTuneCollision, m_Passive, m_PoliceHelper, m_Item, m_DoorHammer, m_AlwaysTeleWeapon, m_FreezeHammer,
 		m_aSpawnWeaponActive[0], m_aSpawnWeaponActive[1], m_aSpawnWeaponActive[2],
 		m_HasFinishedSpecialRace, m_GotMoneyXPBomb, m_SpawnTick, m_KillStreak, m_MaxJumps, m_CarriedFlag,
@@ -533,9 +535,10 @@ int CSaveTee::LoadString(char* String)
 		"%d\t%d\t%d\t"
 		"%d\t%d\t%d\t"
 		"%d\t%d\t%d\t"
+		"%d\t%d\t%d\t"
 		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
-		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
-		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
+		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
+		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
 		"%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"
 		"%d\t%d\t%d\t"
 		"%d\t%d\t%lld\t%d\t%d\t%d\t"
@@ -580,11 +583,12 @@ int CSaveTee::LoadString(char* String)
 		&m_aWeapons[13].m_AmmoRegenStart, &m_aWeapons[13].m_Ammo, &m_aWeapons[13].m_Got,
 		&m_aWeapons[14].m_AmmoRegenStart, &m_aWeapons[14].m_Ammo, &m_aWeapons[14].m_Got,
 		&m_aWeapons[15].m_AmmoRegenStart, &m_aWeapons[15].m_Ammo, &m_aWeapons[15].m_Got,
+		&m_aWeapons[16].m_AmmoRegenStart, &m_aWeapons[16].m_Ammo, &m_aWeapons[16].m_Got,
 		&m_Invisible, &m_Rainbow, &m_Atom, &m_Trail, &m_Meteors, &m_Bloody, &m_StrongBloody, &m_ScrollNinja, &m_HookPower,
 		&m_aSpreadWeapon[0], &m_aSpreadWeapon[1], &m_aSpreadWeapon[2], &m_aSpreadWeapon[3], &m_aSpreadWeapon[4], &m_aSpreadWeapon[5], &m_aSpreadWeapon[6], &m_aSpreadWeapon[7],
-		&m_aSpreadWeapon[8], &m_aSpreadWeapon[9], &m_aSpreadWeapon[10], &m_aSpreadWeapon[11], &m_aSpreadWeapon[12], &m_aSpreadWeapon[13], &m_aSpreadWeapon[14], &m_aSpreadWeapon[15],
+		&m_aSpreadWeapon[8], &m_aSpreadWeapon[9], &m_aSpreadWeapon[10], &m_aSpreadWeapon[11], &m_aSpreadWeapon[12], &m_aSpreadWeapon[13], &m_aSpreadWeapon[14], &m_aSpreadWeapon[15], &m_aSpreadWeapon[16],
 		&m_aHadWeapon[0], &m_aHadWeapon[1], &m_aHadWeapon[2], &m_aHadWeapon[3], &m_aHadWeapon[4], &m_aHadWeapon[5], &m_aHadWeapon[6], &m_aHadWeapon[7],
-		&m_aHadWeapon[8], &m_aHadWeapon[9], &m_aHadWeapon[10], &m_aHadWeapon[11], &m_aHadWeapon[12], &m_aHadWeapon[13], &m_aHadWeapon[14], &m_aHadWeapon[15],
+		&m_aHadWeapon[8], &m_aHadWeapon[9], &m_aHadWeapon[10], &m_aHadWeapon[11], &m_aHadWeapon[12], &m_aHadWeapon[13], &m_aHadWeapon[14], &m_aHadWeapon[15], &m_aHadWeapon[16],
 		&m_FakeTuneCollision, &m_OldFakeTuneCollision, &m_Passive, &m_PoliceHelper, &m_Item, &m_DoorHammer, &m_AlwaysTeleWeapon, &m_FreezeHammer,
 		&m_aSpawnWeaponActive[0], &m_aSpawnWeaponActive[1], &m_aSpawnWeaponActive[2],
 		&m_HasFinishedSpecialRace, &m_GotMoneyXPBomb, &m_SpawnTick, &m_KillStreak, &m_MaxJumps, &m_CarriedFlag,
@@ -621,7 +625,7 @@ int CSaveTee::LoadString(char* String)
 	{
 	case 91:
 		return 0;
-	case 225: // F-DDrace extra vars
+	case 230: // F-DDrace extra vars
 		return 0;
 	default:
 		dbg_msg("load", "failed to load tee-string");
