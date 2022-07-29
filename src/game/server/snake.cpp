dcc4ccf563d6e9b2a3c5477271b06d6d2620c1a7
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2022-07-29 16:41:52 +0200

    disallow picking up people from other snakes

--- src/game/server/snake.cpp
+++ src/game/server/snake.cpp
@@ -125,7 +125,7 @@ void CSnake::AddNewTees()
 	for (int i = 0; i < MAX_CLIENTS; i++)
 	{
 		CCharacter *pChr = GameServer()->GetPlayerChar(i);
-		if (!pChr || IsInSnake(pChr))
+		if (!pChr || IsInAnySnake(pChr))
 			continue;
 
 		if (distance(m_vSnake[0].m_pChr->Core()->m_Pos, pChr->Core()->m_Pos) <= 40.f)
