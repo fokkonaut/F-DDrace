57a8555ec6f555dcc19a3ecb6b44a0e018b3bac3
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2022-09-26 20:54:41 +0200

    sitting nah

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -716,13 +716,11 @@ void CPlayer::Snap(int SnappingClient)
 
 	pDDNetPlayer->m_AuthLevel = GetAuthedHighlighted();
 	pDDNetPlayer->m_Flags = 0;
-
-	bool Sitting = (m_pCharacter && m_pCharacter->m_MoneyTile) && SnappingClient != m_ClientID && GameServer()->GetClientDDNetVersion(SnappingClient) >= VERSION_DDNET_MULTI_LASER;
 	if(m_Afk || GameServer()->Arenas()->IsConfiguring(m_ClientID) || Server()->DesignChanging(m_ClientID))
 		pDDNetPlayer->m_Flags |= EXPLAYERFLAG_AFK;
 	if(m_Paused == PAUSE_SPEC)
 		pDDNetPlayer->m_Flags |= EXPLAYERFLAG_SPEC;
-	if(m_Paused == PAUSE_PAUSED || GameServer()->Arenas()->IsConfiguring(m_ClientID) || Sitting)
+	if(m_Paused == PAUSE_PAUSED || GameServer()->Arenas()->IsConfiguring(m_ClientID))
 		pDDNetPlayer->m_Flags |= EXPLAYERFLAG_PAUSED;
 
 	bool ShowSpec = false;
