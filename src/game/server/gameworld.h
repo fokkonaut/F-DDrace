d65273e2610f301051bfeb3f6756bb15fdcb6711
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2023-03-09 17:32:10 +0100

    make character slightly movable

--- src/game/server/gameworld.h
+++ src/game/server/gameworld.h
@@ -40,6 +40,7 @@ public:
 		ENTTYPE_PROJECTILE = 0,
 		ENTTYPE_LASER,
 		ENTTYPE_PICKUP,
+		ENTTYPE_LIGHTNING_LASER, // put lightning laser before character, so that vel set to vec2(0, 0) will make a chr fall slowly, and make him slightly movable
 		ENTTYPE_CHARACTER,
 		ENTTYPE_FLAG,
 
@@ -69,7 +70,6 @@ public:
 		ENTTYPE_ROTATING_BALL,
 		ENTTYPE_STAFF_IND,
 		ENTTYPE_PORTAL_BLOCKER,
-		ENTTYPE_LIGHTNING_LASER,
 
 		NUM_ENTTYPES
 	};
