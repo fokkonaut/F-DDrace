a086dc55519dd38e8e5321d992e28c1969d5cc5c
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2023-08-05 05:58:55 +0200

    Add tune for elasticity xd

--- src/game/tuning.h
+++ src/game/tuning.h
@@ -98,4 +98,6 @@ MACRO_TUNING_PARAM(MoneyMaxFlySpeed, money_max_fly_speed, 3, "Fly speed for foll
 
 MACRO_TUNING_PARAM(LightningLaserCount, lightning_laser_count, 7, "Number of lasers used for the lightning laser (min: 1)")
 MACRO_TUNING_PARAM(LightningLaserLength, lightning_laser_length, 70, "Length of lasers used for the lightning laser (min: 1)")
+
+MACRO_TUNING_PARAM(Elasticity, elasticity, 0, "Elasticity, bouncing off of blocks")
 #endif
