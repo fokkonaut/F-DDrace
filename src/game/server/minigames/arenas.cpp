d2296c84f8f693a8a489a9ff6b6920b8232f055f
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2024-06-15 21:07:44 +0200

    Add confetti if other player rq 1vs1 too

--- src/game/server/minigames/arenas.cpp
+++ src/game/server/minigames/arenas.cpp
@@ -633,6 +633,7 @@ void CArenas::OnPlayerLeave(int ClientID, bool Disconnect)
 			char aBuf[128];
 			str_format(aBuf, sizeof(aBuf), "'%s' left a 1vs1 round against '%s'! Current scores: %d - %d", Server()->ClientName(ClientID), Server()->ClientName(OtherID), m_aFights[Fight].m_aParticipants[Index].m_Score, m_aFights[Fight].m_aParticipants[Other].m_Score);
 			GameServer()->SendChat(-1, CHAT_ALL, -1, aBuf);
+			GameServer()->m_apPlayers[Other]->m_ConfettiWinEffectTick = Server()->Tick();
 		}
 
 		EndFight(Fight);
