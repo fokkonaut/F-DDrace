935780196353e90a176aeff16015a13584828f5b
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2024-06-17 16:17:24 +0200

    dont crash server on record cmd

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -3284,7 +3284,8 @@ bool CServer::DemoRecorder_IsRecording()
 
 void CServer::ConRecord(IConsole::IResult *pResult, void *pUser)
 {
-	CServer* pServer = (CServer *)pUser;
+	// Crashes, cuz snap functions dont handle clientid -1 properly :) xd
+	/*CServer* pServer = (CServer *)pUser;
 	char aFilename[128];
 	if(pResult->NumArguments())
 		str_format(aFilename, sizeof(aFilename), "demos/%s.demo", pResult->GetString(0));
@@ -3294,7 +3295,7 @@ void CServer::ConRecord(IConsole::IResult *pResult, void *pUser)
 		str_timestamp(aDate, sizeof(aDate));
 		str_format(aFilename, sizeof(aFilename), "demos/demo_%s.demo", aDate);
 	}
-	pServer->m_DemoRecorder.Start(pServer->Storage(), pServer->Console(), aFilename, pServer->GameServer()->NetVersion(), pServer->m_aCurrentMap, pServer->m_CurrentMapSha256, pServer->m_CurrentMapCrc, "server");
+	pServer->m_DemoRecorder.Start(pServer->Storage(), pServer->Console(), aFilename, pServer->GameServer()->NetVersion(), pServer->m_aCurrentMap, pServer->m_CurrentMapSha256, pServer->m_CurrentMapCrc, "server");*/
 }
 
 void CServer::ConStopRecord(IConsole::IResult *pResult, void *pUser)
