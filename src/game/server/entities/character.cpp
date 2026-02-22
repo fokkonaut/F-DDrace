0f0839f4ca8d40b97514cf5762e7bcac1b167edf
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-02-22 15:17:50 +0100

    Fix build

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -3417,8 +3417,9 @@ void CCharacter::HandleTiles(int Index)
 			for (int i = 0; i < MAX_CLIENTS; ++i)
 			{
 				CPlayer *pPlayer = GameServer()->m_apPlayers[i];
-				bool DummiesAsPlayers = (!pPlayer->m_IsDummy && !pPlayer->IsDummy()) || Config()->m_Sv
-				if (pPlayer && pPlayer->GetTeam() != TEAM_SPECTATORS && )
+				bool NoServerBot = !pPlayer->m_IsDummy && !pPlayer->IsDummy();
+				bool AllowDummy = NoServerBot || Config()->m_SvIncreaseHumanCapBots;
+				if (pPlayer && pPlayer->GetTeam() != TEAM_SPECTATORS && AllowDummy)
 				{
 					Players++;
 
