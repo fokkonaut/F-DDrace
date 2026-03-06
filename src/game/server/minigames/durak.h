afd7873e9967af56edbf781eed0cfb45ae564c2e
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-03-06 17:20:50 +0100

    Fix durak pass

--- src/game/server/minigames/durak.h
+++ src/game/server/minigames/durak.h
@@ -712,8 +712,19 @@ public:
 		{
 			if (!m_Attacks[i].m_Offense.Valid())
 			{
-				int NewDefender = GetNextPlayer(m_DefenderIndex, true);
-				if (NewDefender == -1 || (int)m_aSeats[NewDefender].m_Player.m_vHandCards.size() < NumAttacks + 1)
+				int NewDefender = -1;
+				for (int i = 0; i < MAX_DURAK_PLAYERS; i++)
+				{
+					int NextIndex = (m_DefenderIndex + (i + 1) + MAX_DURAK_PLAYERS) % MAX_DURAK_PLAYERS;
+					if (m_aSeats[NextIndex].m_Player.m_ClientID != -1 && m_aSeats[NextIndex].m_Player.m_Stake >= 0 &&
+						(int)m_aSeats[NextIndex].m_Player.m_vHandCards.size() >= NumAttacks + 1)
+					{
+						NewDefender = NextIndex;
+						break;
+					}
+				}
+
+				if (NewDefender == -1)
 					return -1;
 
 				m_Attacks[i].m_Offense.m_Suit = pCard->m_Suit;
