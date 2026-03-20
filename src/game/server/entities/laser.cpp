fokkonaut/F-DDrace

commit 5e5aa8cad853c5b04e657aee90217de353f7e2b7

Fix healing helicopter

src/game/server/entities/laser.cpp
@@ -203,10 +203,6 @@ bool CLaser::HitEntity(vec2 From, vec2 To)
 			vec2 Pos = At + normalize(At - From) * vec2(-32.f, -32.f);
 			GameServer()->CreateExplosion(Pos, m_Owner, WEAPON_LASER, true, pOwnerChar ? pOwnerChar->Team() : pChr->Team(), m_TeamMask);
 		}
-		else if (pChr)
-		{
-			LaserHitCharacter(pChr);
-		}
 		else if (pEnt && pEnt->GetObjType() == CGameWorld::ENTTYPE_HELICOPTER)
 		{
 			CHelicopter* pHelicopter = (CHelicopter*)pEnt;
@@ -227,6 +223,12 @@ bool CLaser::HitEntity(vec2 From, vec2 To)
 
 			return true;
 		}
+
+		if (pChr)
+		{
+			LaserHitCharacter(pChr);
+		}
+
 	}
 	else if (m_Type == WEAPON_TASER)
 	{

commit 803decc749fee6974001d79dce05115d688eb651

Fix laser unfreezing zombies, caused by 5e5aa8c

src/game/server/entities/laser.cpp
@@ -198,12 +198,7 @@ bool CLaser::HitEntity(vec2 From, vec2 To)
 	}
 	else if (m_Type == WEAPON_LASER)
 	{
-		if (IsCharacter && pChr->m_IsZombie)
-		{
-			vec2 Pos = At + normalize(At - From) * vec2(-32.f, -32.f);
-			GameServer()->CreateExplosion(Pos, m_Owner, WEAPON_LASER, true, pOwnerChar ? pOwnerChar->Team() : pChr->Team(), m_TeamMask);
-		}
-		else if (pEnt && pEnt->GetObjType() == CGameWorld::ENTTYPE_HELICOPTER)
+		if (pEnt && pEnt->GetObjType() == CGameWorld::ENTTYPE_HELICOPTER)
 		{
 			CHelicopter* pHelicopter = (CHelicopter*)pEnt;
 			pHelicopter->Heal(1.0f);
@@ -224,7 +219,12 @@ bool CLaser::HitEntity(vec2 From, vec2 To)
 			return true;
 		}
 
-		if (pChr)
+		if (IsCharacter && pChr->m_IsZombie)
+		{
+			vec2 Pos = At + normalize(At - From) * vec2(-32.f, -32.f);
+			GameServer()->CreateExplosion(Pos, m_Owner, WEAPON_LASER, true, pOwnerChar ? pOwnerChar->Team() : pChr->Team(), m_TeamMask);
+		}
+		else if (pChr)
 		{
 			LaserHitCharacter(pChr);
 		}