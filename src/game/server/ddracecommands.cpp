9de87adb22fe4158c430a719c7bc7896f640a3dd
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-04-18 16:09:49 +0200

    Missing print in playerinfo for design

--- src/game/server/ddracecommands.cpp
+++ src/game/server/ddracecommands.cpp
@@ -1183,6 +1183,7 @@ void CGameContext::ConPlayerInfo(IConsole::IResult *pResult, void *pUserData)
 		pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "console", aBuf);
 	}
 	str_format(aBuf, sizeof(aBuf), "Design: %s", pSelf->Server()->GetMapDesign(ID));
+	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "console", aBuf);
 	str_format(aBuf, sizeof(aBuf), "Language: %s", g_Localization.GetLanguageString(pPlayer->m_Language));
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "console", aBuf);
 	if (pChr)
