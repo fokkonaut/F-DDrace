c41aa245b1bfacb702a460a412aa182509112f1a
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-04-23 02:27:19 +0200

    Swap drive and boost

--- src/game/server/entities/vehicle/spider.cpp
+++ src/game/server/entities/vehicle/spider.cpp
@@ -39,8 +39,8 @@ void CSpider::HandleSeat(SSeat& Seat, int PassengerCID, CCharacter *pChar)
 		if (!pChar->m_FreezeTime)
 		{
 			SSeat::SInputs& Inputs = Seat.m_Inputs;;
-			bool Drive = Inputs.m_Jump % 2 == 1;
-			bool Boost = Inputs.m_Fire % 2 == 1;
+			bool Drive = Inputs.m_Fire % 2 == 1;
+			bool Boost = Inputs.m_Jump % 2 == 1;
 			if (Drive && Inputs.m_MouseX && Inputs.m_MouseY)
 			{
 				vec2 Direction = normalize(vec2((float)Inputs.m_MouseX, (float)Inputs.m_MouseY));
