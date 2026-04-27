50d28fc14aa65330578d99ee2f89dd69d9e43faa
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-04-27 20:51:02 +0200

    Remove default for versioncheck

--- src/engine/shared/config_variables.h
+++ src/engine/shared/config_variables.h
@@ -129,7 +129,7 @@ MACRO_CONFIG_INT(SvSpamMuteDuration, sv_spam_mute_duration, 60, 0, 3600, CFGFLAG
 MACRO_CONFIG_INT(SvRconVote, sv_rcon_vote, 0, 0, 1, CFGFLAG_SERVER, "Only allow authed clients to call votes", AUTHED_ADMIN)
 MACRO_CONFIG_INT(SvSpectatorVotes, sv_spectator_votes, 1, 0, 1, CFGFLAG_SERVER, "Choose if spectators are allowed to start votes", AUTHED_ADMIN)
 MACRO_CONFIG_INT(SvShutdownWhenEmpty, sv_shutdown_when_empty, 0, 0, 1, CFGFLAG_SERVER, "Shutdown server as soon as no one is on it anymore", AUTHED_ADMIN)
-MACRO_CONFIG_STR(SvBannedVersions, sv_banned_versions, 128, "1,16,308,405,502,602,604,605,666,708,1003,1661,2773,10072,11017,11072,12073", CFGFLAG_SERVER, "Comma separated list of banned clients to be kicked on join", AUTHED_ADMIN)
+MACRO_CONFIG_STR(SvBannedVersions, sv_banned_versions, 128, "", CFGFLAG_SERVER, "Comma separated list of banned clients to be kicked on join", AUTHED_ADMIN)
 
 // netlimit
 MACRO_CONFIG_INT(SvNetlimit, sv_netlimit, 0, 0, 10000, CFGFLAG_SERVER, "Netlimit: Maximum amount of traffic a client is allowed to use (in kb/s)", AUTHED_ADMIN)
