3a73b984bcc8ee8eeb4aa2539e28b16569bb0b63
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-04-27 23:55:00 +0200

    Decrease portal blocker price and level

--- src/game/server/houses/shop.cpp
+++ src/game/server/houses/shop.cpp
@@ -35,7 +35,7 @@ CShop::CShop(CGameContext *pGameServer, int Type) : CHouse(pGameServer, Type)
 		AddItem("Taser", 30, -1, TIME_FOREVER, Localizable("Taser is a rifle that freezes a player. For more information about the taser and your taser stats, please visit '/taser'."));
 		AddItem("Taser battery", 30, 100000, TIME_FOREVER, Localizable("Taser battery is required to use the taser. Maximum amount of ammo is 100. Please visit '/taser'."), false, 10);
 		AddItem("Portal Rifle", EuroMode ? 1 : 45, EuroMode ? 10 : 1000000, TIME_20_DAYS, Localizable("With Portal Rifle you can create two portals where your cursor is, then teleport between them."), EuroMode);
-		AddItem("Portal Blocker", 20, 10000, TIME_FOREVER, Localizable("Create portal blockers hammer and this ammo with your cursor. See '/portal' for your current amount. How it works: '/helptoggle'"), false, 10);
+		AddItem("Portal Blocker", 15, 7500, TIME_FOREVER, Localizable("Create portal blockers hammer and this ammo with your cursor. See '/portal' for your current amount. How it works: '/helptoggle'"), false, 10);
 		AddItem("Projectile Hammer", 1, 10000, TIME_DISCONNECT, Localizable("Using this item you can redirect projectiles by hitting them with your hammer. There is a chance of losing it on death."));
 
 		static char aaBuf[NUM_POLICE_LEVELS][32];
