c363afd7d06290f6f0407452964ac345d4e4e675
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-05-02 09:53:09 +0200

    Move draweditor and snake to character folder

--- src/game/server/entities/character.h
+++ src/game/server/entities/character.h
@@ -10,8 +10,8 @@
 #include <game/server/entity.h>
 #include <game/server/eventhandler.h>
 #include <game/server/player.h>
-#include <game/server/misc/draweditor.h>
-#include <game/server/misc/snake.h>
+#include <game/server/misc/character/draweditor.h>
+#include <game/server/misc/character/snake.h>
 
 #include "map/pickup.h"
 #include "misc/stable_projectile.h"
