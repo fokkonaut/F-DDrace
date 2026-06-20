a9578a2dec38c6aeae60834680caf332f87a42b3
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-06-20 07:47:10 +0200

    Add null check in http response

--- src/engine/shared/http.cpp
+++ src/engine/shared/http.cpp
@@ -553,6 +553,8 @@ json_value *CHttpRequest::ResultJson() const
 	unsigned char *pResult;
 	size_t ResultLength;
 	Result(&pResult, &ResultLength);
+	if(!pResult || ResultLength == 0)
+		return nullptr;
 	return json_parse((char *)pResult, ResultLength);
 }
 
