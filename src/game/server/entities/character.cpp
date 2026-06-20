b0ed8a6af38f5b2916b37f10789367fa9e12318a
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2026-06-20 09:13:08 +0200

    Fix oob

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1374,7 +1374,7 @@ void CCharacter::HandleWeapons()
 	FireWeapon();
 
 	// ammo regen
-	int AmmoRegenTime = g_pData->m_Weapons.m_aId[GetActiveWeapon()].m_Ammoregentime;
+	int AmmoRegenTime = g_pData->m_Weapons.m_aId[GameServer()->GetWeaponType(GetActiveWeapon())].m_Ammoregentime;
 	if (GetActiveWeapon() == WEAPON_HEART_GUN || GetActiveWeapon() == WEAPON_PROJECTILE_RIFLE)
 		AmmoRegenTime = g_pData->m_Weapons.m_aId[WEAPON_GUN].m_Ammoregentime;
 	if (AmmoRegenTime && m_pPlayer->m_Gamemode == GAMEMODE_VANILLA && !m_FreezeTime && m_aWeapons[GetActiveWeapon()].m_Ammo != -1)
